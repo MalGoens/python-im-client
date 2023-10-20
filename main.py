37a68e28bf829f93a8dc65ebcab4bf5af90cd03c
MalGoens <143752876+MalGoens@users.noreply.github.com>
2023-10-20 16:36:36 +0000

    Added some comments, no actual code as of yet.

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-# Placeholder
+# Copyright Malakii Goens.
+# This code is under the GNU General Public License v3. This code may not be distributed as closed-source.
+
+# Placeholder until I have time to work on this :/
